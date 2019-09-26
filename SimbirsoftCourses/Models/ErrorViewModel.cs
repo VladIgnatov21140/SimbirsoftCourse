c331fcfefac7b664247e9d8e11ca3d21a61343ea
q.4da@yandex.ru <q.4da@yandex.ru>
2019-09-26 17:22:04 +0400

    Создана Болванка

---
+++ SimbirsoftCourses/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SimbirsoftCourses.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
